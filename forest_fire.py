5d11813c9d7696ebbb4c2d1e808d279c1372f6cf
amogh-08 <91011045+amogh-08@users.noreply.github.com>
2021-09-20 14:33:33 +0530

    Update forest_fire.py

--- forest_fire.py
+++ forest_fire.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import warnings
 import pickle
@@ -17,8 +17,7 @@ y = y.astype('int')
 X = X.astype('int')
 # print(X,y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-log_reg = LogisticRegression()
-
+log_reg = linear_model.LogisticRegression()
 
 log_reg.fit(X_train, y_train)
 
